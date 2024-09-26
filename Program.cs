50ed3ef07376011306ec4cc963bb5e5a59b481aa
Dheeraj kumar <dheerajkumar14.a@gmail.com>
2024-09-26 19:31:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 0 || num == 1)
+            { Console.WriteLine(num + " is not a prime number"); }
+            else
+            {
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    { Console.WriteLine(num + " is not a Prime Number"); return; }
+                }
+                Console.WriteLine(num + " is a Prime Number");
+            }
+        }
+    }
+}
